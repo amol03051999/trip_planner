a422961dada5bbadb8ddda6b9e9309c25db469ba
amol shinde <amolsh249@gmail.com>
2025-11-17 21:52:01 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,10 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { viteStaticCopy } from "vite-plugin-static-copy"
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects",
-          dest: ".",    // copy to dist/
-        }
-      ]
-    })
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
